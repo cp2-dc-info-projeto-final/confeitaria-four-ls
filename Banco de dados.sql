d9c746eeb1e91ab74a022fafb2dc381f1850f844
leticia barroso <labcaxias@CDCLAB_13.labdc.zentyal>
2019-06-26 08:06:52 -0300

    Completando banco de dados

--- Banco de dados.sql
+++ Banco de dados.sql
@@ -1,6 +1,7 @@
 USE confeitariafourls;
 
 CREATE TABLE cliente(
+
 id_cliente int,
 CPF varchar(20) ,
 nome varchar (50),
@@ -17,10 +18,57 @@ PRIMARY KEY(id_cliente)
 
 );
 
-CREATE TABLE admnistrador (
+CREATE TABLE admnistrador(
+
 id_administrador int,
 nome varchar(50),
 cod_func varchar(50),
 PRIMARY KEY(id_administrador)
 
+);
+
+CREATE TABLE contato(
+
+    id_contato INT,
+    num_contato VARCHAR (11),
+    endereco_cont VARCHAR (50),
+    emanil-cont VARCHAR (50),
+    PRIMARY KEY(id_contato)
+);
+
+CREATE TABLE produto(
+
+id_produto INT,
+cod_produto INT,
+sabor VARCHAR (50),
+tamanho INT,
+preco FLOAT,
+PRIMARY KEY(id_produto)
+
+);
+
+CREATE TABLE cardapio(
+
+    id_cardapio INT,
+    descricao VARCHAR (1000),
+    PRIMARY KEY(id_cardapio)
+);
+
+CREATE TABLE promocao(
+
+    id_promo INT,
+    data_in DATE,
+    data_fim DATE,
+    desconto FLOAT,
+    PRIMARY KEY (id_promo)
+
+);
+
+CREATE TABLE carrinho(
+
+    id_carrinho INT,
+    id_produto INT,
+    valor_total FLOAT,
+    PRIMARY KEY (id_carrinho),
+    FOREIGN KEY (id_produto)
 );
