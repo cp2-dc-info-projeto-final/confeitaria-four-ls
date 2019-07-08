9c96562ecbf04de5682793b8ed60299a601d6953
leticia barroso <labcaxias@CDCLAB_13.labdc.zentyal>
2019-07-08 08:08:07 -0300

    adicionando cadastro php

---
+++ cadastro.php
@@ -0,0 +1,44 @@
+<?php
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $confirmaSenha = $_POST["confirmaSenha"];
+    $sexo = $_POST["sexo"];
+	$telefone = $sexo = $_POST["telefone"];
+	$cidade = $_POST["cidade"];
+	$endereco = $_POST["endereco"];
+    session_start();
+    if ($senha != $confirmaSenha) {    
+        $erro = "As senhas não são as mesmas";        
+        $_SESSION["erro"] = $erro;
+        header("Location: formCadastro.php");
+        exit();
+    }
+    $hash = password_hash($senha, PASSWORD_DEFAULT);
+    $connection = mysqli_connect("localhost", "root", "", "confeitariafourls");
+ 
+    if($connection === false){
+        die("Erro" . mysqli_connect_error());
+    }
+    $sql = "SELECT id_cliente FROM cliente WHERE email='$email'";
+    $result = mysqli_query($connection, $sql);
+    $erro = "";
+    
+    if (mysqli_num_rows($result) > 0) {
+        $erro = "E-mail indisponível";        
+        $_SESSION["erro"] = $erro;
+        header("Location: formCadastro.php");
+        exit();
+    }
+    $sql = "INSERT INTO cliente (nome, email, senha, sexo, telefone, cidade, endereço)
+    VALUES ('$nome', '$email', '$senha','$sexo','$telefone','$cidade', '$endereco')";
+    if(mysqli_query($connection, $sql)){
+        session_unset();
+        header("Location: formLogin.php");
+        exit();
+    } else{
+        die("Erro $sql. " . mysqli_error($connection));
+    }
+    mysqli_close($connection);
+?>
+
