349edbf4a341a8f937729065b3f49b853bb66fc0
leticia barroso <labcaxias@CDCLAB_13.labdc.zentyal>
2019-07-10 07:22:50 -0300

    alterando php login

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php 
-$login = $_POST['login'];
-$entrar = $_POST['entrar'];
-$senha = md5($_POST['senha']);
+$login = $_POST["login"];
+$entrar = $_POST["entrar"];
+$senha = md5($_POST["senha"]);
 $connect = mysql_connect("localhost", "root", "", "confeitariafourls");
 $db = mysql_select_db('confeitariafourls');
   if (isset($entrar)) {
