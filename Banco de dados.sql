b1f2b6e5a9a3b0444f8dbf39fb88d2b5bfd7315e
larabms <lahbms2003@gmail.com>
2019-10-09 08:28:17 -0300

    alterando código

--- Banco de dados.sql
+++ Banco de dados.sql
@@ -81,8 +81,8 @@ FOREIGN KEY(id_produto) REFERENCES produto(id_produto)
 );
 DROP TABLE IF EXISTS produto_venda;
 CREATE TABLE produto_venda(
-id_venda INT NOT NULL PRIMARY KEY,
-id_produto INT NOT NULL PRIMARY KEY,
+id_venda INT,
+id_produto INT,
 qtd INT,
 FOREIGN KEY(id_produto) REFERENCES produto(id_produto),
 FOREIGN KEY(id_venda) REFERENCES venda(id_venda)
