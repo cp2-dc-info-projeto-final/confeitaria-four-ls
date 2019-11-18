beb827b6a260a29328613ed6f44f1c84079c86d8
larabms <lahbms2003@gmail.com>
2019-11-18 13:33:48 -0300

    alterando conexão

--- paginacliente/carrinho/connection.php
+++ paginacliente/carrinho/connection.php
@@ -6,7 +6,7 @@
     define("DB_NAME", "confeitariafourls");
     define("DB_USER", "root");
     define("DB_PASS", "");
-
     //Conexao com Banco de Dados
     return new PDO('mysql:host=localhost;dbname=confeitariafourls',"root","");
+
  }
