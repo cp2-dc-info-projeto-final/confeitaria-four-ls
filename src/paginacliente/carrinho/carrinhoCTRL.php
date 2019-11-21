6b988b9e13c8bf7ef094abb28c02bc3a8bfc2175
lucas0108 <49063874+lucas0108@users.noreply.github.com>
2019-11-20 21:15:16 -0300

    modificação pra reconhecimento do cliente

--- src/paginacliente/carrinho/carrinhoCTRL.php
+++ src/paginacliente/carrinho/carrinhoCTRL.php
@@ -1,21 +1,25 @@
 <?php
 
     Require "carrinhoModel.php";
-    
-    $qtd = $_POST["qtd"];
     session_start();
-
-  $id_usuario = $_SESSION["id_cliente"];
-  $id_produto = $_SESSION["id"];
+  $id_usuario = $_SESSION['id_cliente'];
+  $id_produto = $_SESSION['id'];
+  $qtd = $_SESSION['qtd'];
     
-        if (criarvenda($id_cliente, $data_venda,$hora_venda)) {
-            session_unset();
-            header("Location: ../paginacliente.php");
-            exit();
+    if (isset($_SESSION['id_cliente']){
+        if (criarvenda($id_cliente)) {
+            if(addprodven($id_produto, $qtd)){
+                session_unset();
+                header("Location: ../paginacliente.php");
+                exit();
+            }
         } else {
             $erro = "Não foi possivel realizar o pedido";        
             $_SESSION["erro"] = $erro;
             header("Location: carrinho.php");
             exit();
         }
+    } else {
+        header("Location: ../../login/login.php")
+    }
 ?>
