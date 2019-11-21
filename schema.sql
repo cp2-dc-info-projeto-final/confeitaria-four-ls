21b32c8aff12947824d43529d4f66bea3446a418
leticiabarrosoo <leticiabarroso4@gmail.com>
2019-11-21 12:58:38 -0300

    alterando banco de dados

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS cliente;
 CREATE TABLE cliente(
 id_cliente INT NOT NULL PRIMARY KEY,
 nome VARCHAR (50),
-cpf VARCHAR (11)
+cpf VARCHAR (11),
 sexo VARCHAR (10),
 telefone VARCHAR (11),
 endereco VARCHAR (100),
@@ -33,18 +33,19 @@ id_produto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 nomepro VARCHAR (50),
 descricao VARCHAR(300),
 preco FLOAT,
-imagem VARCHAR(250)
+imagem VARCHAR(250),
+visible BOOLEAN NOT NULL
 );
 
-INSERT INTO produto (nomepro, descricao, preco, imagem)
-VALUES ("Bolo de Cenoura", "Delicioso bolo de cenoura com cobertura de chocolate. Rende 10 porções.", 20.00, "../Imagens_Produtos/1/bolocenoura.jpg")
-VALUES ("Bem Casados", "Preço por unidade",1.20, "../Imagens_Produtos/2/bemcasados.jpg")
-VALUES ("Bolo de Brigadeiro", "Bolo de chocolate com brigadeiro. Rende 10 porçôes",20.00 , "../Imagens_Produtos/3/bolodebrigadeiro.jpg")
-VALUES ("Pavê", "Rende 6 porções", 15.00 ,"../Imagens_Produtos/4/pave.jpg")
-VALUES ("Macarons Recheados", "Preço por unidade", 1.50 , "../Imagens_Produtos/5/macaronsrecheados.jpg")
-VALUES ("CakePop", "Delicioso bolo no palito. Preço por unidade", 1.00 , "../Imagens_Produtos/6/cakepop.jpg")
-VALUES ("Doces Finos", "Doces finos para festas. Preço por unidade",2.00, "../Imagens_Produtos/7/docesfinos.jpg")
-VALUES ("Ovo Recheado", "Ovo recheado com chocolate e frutas",30.00 , "../Imagens_Produtos/8/ovorecheado.jpg")
+INSERT INTO produto (nomepro, descricao, preco, imagem, visible)
+VALUES ("Bolo de Cenoura", "Delicioso bolo de cenoura com cobertura de chocolate. Rende 10 porções.", 20.00, "bolocenoura.jpg", 1),
+ ("Bem Casados", "Preço por unidade",1.20, "bemcasados.jpg", 1),
+ ("Bolo de Brigadeiro", "Bolo de chocolate com brigadeiro. Rende 10 porçôes",20.00 , "bolodebrigadeiro.jpg", 1),
+ ("Pavê", "Rende 6 porções", 15.00 ,"pave.jpg", 1),
+ ("Macarons Recheados", "Preço por unidade", 1.50 , "macaronsrecheados.jpg", 1),
+ ("CakePop", "Delicioso bolo no palito. Preço por unidade", 1.00 , "cakepop.jpg", 1),
+ ("Doces Finos", "Doces finos para festas. Preço por unidade",2.00, "docesfinos.jpg", 1),
+ ("Ovo Recheado", "Ovo recheado com chocolate e frutas",30.00 , "ovorecheado.jpg", 1);
 DROP TABLE IF EXISTS cliente_produto;
 CREATE TABLE cliente_produto(
 id_cliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -67,4 +68,6 @@ qtd INT,
 FOREIGN KEY(id_produto) REFERENCES produto(id_produto),
 FOREIGN KEY(id_venda) REFERENCES venda(id_venda)
 );
+
+
 
