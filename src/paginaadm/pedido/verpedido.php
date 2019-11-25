c1a1665fc88d50a711c149e90b4491f3fc732c0e
leticiabarrosoo <leticiabarroso4@gmail.com>
2019-11-25 08:00:15 -0300

    adicionando cógido para teste

--- src/paginaadm/pedido/verpedido.php
+++ src/paginaadm/pedido/verpedido.php
@@ -1,6 +1,5 @@
 <?php
-require "../../connection.php";
-
+ require_once "connection.php";
 
  function buscapedidos() {
     
@@ -8,15 +7,47 @@ require "../../connection.php";
     if ($conn===false){
         die("Erro de conexão".mysqli_connect_error());
     }
-    $sql = "SELECT cliente.nome, cliente.telefone, produto.nomepro, produto.preco, venda.data_venda, produto_venda.qtd FROM cliente JOIN venda on cliente.id_cliente = venda.id_cliente JOIN produto_venda	ON produto_venda.id_venda = venda.id_venda
-    JOIN produto ON produto.id_produto = produto_venda.id_produto";
+    $sql = "SELECT 
+                v.id,
+                v.idusuario,
+                v.dataa,
+                v.vtotal,
+                u.nome,
+                u.email,
+                u.endereco
+            FROM venda as v
+            JOIN usuario as u ON v.idusuario = u.id";
     $result = mysqli_query($conn, $sql);
     return mysqli_fetch_all($result, MYSQL_ASSOC);
  }
 
 
+ function buscaritens($id_venda) {
+    
+    $conn=get_connection();
+    if ($conn===false){
+        die("Erro de conexão".mysqli_connect_error());
+    }
+    $sql = " SELECT 
+                v.id,
+                v.idusuario,
+                v.dataa,
+                v.vtotal,
+                p.nome,
+                p.preco,
+                p.descricao,
+                pv.qtd
+            FROM venda as v
+            JOIN produtovenda as pv ON v.id = pv.id_venda
+            JOIN produto as p ON p.id = pv.id_produto
+            WHERE v.id = $id_venda";
+    $result = mysqli_query($conn, $sql);
+    return mysqli_fetch_all($result, MYSQL_ASSOC);
+ }
 
- $vendas = buscapedidos();
+ 
+
+ $vendas = buscarvendas();
 
  foreach($vendas as $venda) {
 
@@ -35,7 +66,7 @@ require "../../connection.php";
             <td> <?php echo $venda["nome"]; ?> </td>
             <td> <?php echo $venda["email"]; ?> </td>
             <td> <?php echo $venda["endereco"]; ?> </td>
-            <td> <?php echo $venda["data_venda"]; ?>  </td>
+            <td> <?php echo $venda["dataa"]; ?>  </td>
             <td> R$ <?php echo number_format ($venda['vtotal'], 2, ',', '.')?></td>
       
             <tr>
