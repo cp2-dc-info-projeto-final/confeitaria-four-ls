ba53d6147c45401da136ed4065cb25b0c70b3338
larabms <lahbms2003@gmail.com>
2019-11-26 14:51:42 -0300

    correção de caracteres especiais no cadastro de produto

--- src/paginaadm/cardapio_model.php
+++ src/paginaadm/cardapio_model.php
@@ -1,16 +1,40 @@
 <?php
+require_once "../connection.php";
+
 function cadastro($nomepro, $imagem, $preco, $descricao){
-    $conn = mysqli_connect("localhost", "root", "", "confeitariafourls");
-    mysqli_set_charset ($conn , "utf-8" );
-    $sql="INSERT INTO produto(nomepro, imagem, preco, descricao, visible) VALUES ('$nomepro', '$imagem', '$preco', '$descricao', 1)";
-    if(!mysqli_query($conn, $sql)) {
-        die("Erro $sql. " . mysqli_error($conn));
+    $conn = getConnection();
+    
+
+    try {
+        $sql="INSERT INTO produto(nomepro, imagem, preco, descricao, visible) VALUES 
+        (:nomepro, :imagem, :preco, :descricao, 1)";
+
+        $query = $conn->prepare($sql);
+        $query->bindValue(':nomepro', $nomepro);
+        $query->bindValue(':imagem', $imagem);
+        $query->bindValue(':preco', $preco);
+        $query->bindValue(':descricao', $descricao);
+
+        $query->execute();
+
+        $id = $conn->lastInsertId();
+    } catch(PDOExecption $e) {
+        print "Error!: $sql" . $e->getMessage() . "</br>";
     } 
-    $id = mysqli_insert_id($conn);
+    
+    try {
+        $sql="UPDATE produto 
+                SET imagem='Imagens_Produtos/:id/:imagem'
+                WHERE id_produto=:id";
+
+        $query = $conn->prepare($sql);
+        $query->bindValue(':imagem', $imagem);
+        $query->bindValue(':id', $id);
+        $query->execute();
+    } catch(PDOExecption $e) {
+        print "Error!: $sql" . $e->getMessage() . "</br>";
+    } 
     return $id;
-    $sql="UPDATE produto SET imagem='Imagens_Produtos/$id/$imagem'
-                WHERE id_produto=$id";
-                mysqli_query($conn, $sql);
 }
 
 ?>
